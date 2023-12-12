7baf3d5739355d598911673dd6b9a4f7aa7f4202
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-12 11:46:49 +0000

    malloc

--- estudiante.c
+++ estudiante.c
@@ -1,8 +1,20 @@
 #include <string.h>
 #include "estudiante.h"
+#include <stdlib.h>
 
-struct Estudiante estudiante1;
+int main() {
+struct Estudiante *estudiante1 = malloc(sizeof(struct Estudiante));
+
+if (estudiante1 == NULL) {
+    printf("No se pudo asignar memoria");
+    return 1;
+}
 
 strcpy(estudiante1.nombre, "Juan");
 estudiante1.edad = 20;
 estudiante1.promedio = 9.5;
+
+free(estudiante1);
+
+return 0;
+}
