a981e042e9232cd8bf1ab6430f536ace6a9d59ef
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-12 11:59:03 +0000

    ejecucion copiar

--- copiarestudiante.h
+++ copiarestudiante.h
@@ -8,3 +8,14 @@ struct Estudiante copiarEstudiante(struct Estudiante est) {
     copia.promedio = est.promedio;
     return copia;
 }
+
+int main() {
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    struct Estudiante estudiante2 = copiarEstudiante(estudiante1);
+
+    return 0;
+}
