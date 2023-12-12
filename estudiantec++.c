627428f0ed85ea9d135c5dccd3e740a92e90705b
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-12 12:08:50 +0000

    imprimir

--- estudiantec++.c
+++ estudiantec++.c
@@ -6,3 +6,16 @@ public:
     std::string nombre;
     int edad;
     float promedio;
+
+    Estudiante(std::string nombre, int edad, float promedio) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->promedio = promedio;
+    }
+
+        void imprimir() {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Promedio: " << promedio << std::endl;
+    }
+};
