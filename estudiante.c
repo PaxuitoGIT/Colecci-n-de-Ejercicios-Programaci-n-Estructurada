811808da3c237a7145784e306a81a5203344e4cf
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-12 11:42:29 +0000

    instanciacion

---
+++ estudiante.c
@@ -0,0 +1,8 @@
+#include <string.h>
+#include "estudiante.h"
+
+struct Estudiante estudiante1;
+
+strcpy(estudiante1.nombre, "Juan");
+estudiante1.edad = 20;
+estudiante1.promedio = 9.5;
--- estudiante.h
+++ estudiante.h
@@ -3,9 +3,3 @@ struct Estudiante {
     int edad;
     float promedio;
 };
-
-struct Estudiante estudiante1;
-
-strcpy(estudiante1.nombre, "Juan");
-estudiante1.edad = 20;
-estudiante1.promedio = 9.5;
