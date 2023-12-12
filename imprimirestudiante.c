080719713e822ac7427ea20bd74614253af64faa
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-12 12:03:28 +0000

    ejecucion imprimir

--- imprimirestudiante.c
+++ imprimirestudiante.c
@@ -6,3 +6,14 @@ void imprimirEstudiante(Estudiante est) {
     printf("Edad: %d\n", est.edad);
     printf("Promedio: %.2f\n", est.promedio);
 }
+
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
